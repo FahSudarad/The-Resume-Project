3b066d6a1494359713c8b146ebe72b46dbd6e00b
Fah <103676512+FahSudarad@users.noreply.github.com>
2023-03-08 20:57:04 +0700

    Update workExperience.jsx

--- the-resume-project/src/data/workExperience.jsx
+++ the-resume-project/src/data/workExperience.jsx
@@ -4,21 +4,21 @@ const workExperience = [
     name: "co-operative education",
     description:
       "I am responsible for managing a website designed for O-NET Online examination, which uses PHP and phpMyAdmin to collect database information of all users and record students' scores. Additionally, I utilized CSS and JavaScript to enhance the website's visual design and functionality.",
-    img: "./src/pictures/co-operative.jpg",
+    img: "https://imgz.io/images/2023/03/08/co-operative.jpeg",
   },
   {
     id: 2,
     name: "Studio33 — IT Support",
     description:
       "I possess more than 3 years of experience in outsourced IT support and am proficient in building websites using PHP, CSS, and JavaScript. In my role, I am responsible for designing and developing websites that meet the needs of clients and exceed their expectations.",
-    img: "./src/pictures/Support.jpg",
+    img: "https://sv1.picz.in.th/images/2023/03/08/eQlz4g.jpg",
   },
   {
     id: 3,
     name: "T-NET Co,Ltd — IT Security Consultant",
     description:
       "I work as an Information Security Consultant and Internal Auditor, specializing in implementing ISO 27001 and PDPA regulations.",
-    img: "./src/pictures/Consultant.jpg",
+    img: "https://imgz.io/images/2023/03/08/Consultant.jpeg",
   },
 ];
 export default workExperience;

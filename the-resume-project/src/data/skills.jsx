a3a923ec32c1d9d8999279dd9c5a20a9778925fb
Fah <103676512+FahSudarad@users.noreply.github.com>
2023-03-08 20:33:11 +0700

    Update skills.jsx

--- the-resume-project/src/data/skills.jsx
+++ the-resume-project/src/data/skills.jsx
@@ -3,7 +3,7 @@ const skills = [
   { img: "./src/pictures/css.png", skill: "CSS" },
   { img: "./src/pictures/git.png", skill: "GIT" },
   { img: "./src/pictures/react.png", skill: "REACT" },
-  { img: "./src/pictures/api.png", skill: "RESTFUL API" },
+  { img: "https://sv1.picz.in.th/images/2023/03/08/eQjKME.png", skill: "RESTFUL API" },
   { img: "./src/pictures/database.png", skill: "DATABASE" },
 ];
 export default skills;

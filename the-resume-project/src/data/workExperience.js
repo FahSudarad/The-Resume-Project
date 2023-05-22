86c96964b68294955e2638c48b41476cee48c4bb
Fah <103676512+FahSudarad@users.noreply.github.com>
2023-05-22 21:42:37 +0700

    Update workExperience.js

--- the-resume-project/src/data/workExperience.js
+++ the-resume-project/src/data/workExperience.js
@@ -4,7 +4,7 @@ const workExperience = [
     name: "co-operative education",
     description:
       "I am responsible for managing a website designed for O-NET Online examination, which uses PHP and phpMyAdmin to collect database information of all users and record students' scores. Additionally, I utilized CSS and JavaScript to enhance the website's visual design and functionality.",
-    img: "https://imgz.io/images/2023/03/08/co-operative.jpeg",
+    img: "https://imgz.io/images/2023/05/22/co-operative.jpeg",
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ const workExperience = [
     name: "T-NET Co,Ltd — IT Security Consultant",
     description:
       "I work as an Information Security Consultant and Internal Auditor, specializing in implementing ISO 27001 and PDPA regulations.",
-    img: "https://imgz.io/images/2023/03/08/Consultant.jpeg",
+    img: "https://imgz.io/images/2023/05/22/Consultant.jpeg",
   },
 ];
 export default workExperience;
